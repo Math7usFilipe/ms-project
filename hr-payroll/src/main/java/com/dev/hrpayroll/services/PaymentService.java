3b2a73d01edc16ab9b9b6d94cb7ad5f336ac28b2
Math7usFilipe <mxtheus829melo@gmail.com>
2025-01-10 21:21:18 -0300

    hr-payroll implementation mock

---
+++ hr-payroll/src/main/java/com/dev/hrpayroll/services/PaymentService.java
@@ -0,0 +1,12 @@
+package com.dev.hrpayroll.services;
+
+import org.springframework.stereotype.Service;
+
+import com.dev.hrpayroll.entities.Payment;
+
+@Service
+public class PaymentService {
+	public Payment getPayment(long workerId, int days) {
+		return new Payment("Matheus", 50.0, days);
+	}
+}
